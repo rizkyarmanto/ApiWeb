e6e3c4fdc22bf60b85822e0385130df82e4aa6db
unknown <rizkyarmanto0106@gmail.com>
2021-08-27 10:38:32 +0700

    update heroku

---
+++ WebAbsensi/serializers.py
@@ -0,0 +1,23 @@
+from master.models import *
+from rest_framework import serializers
+
+# Serializers define the API representation.
+class MasterSiswaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MasterSiswa
+        fields = ['nisn', 'name', 'username', 'password']
+
+class MasterKelasSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MasterKelas
+        fields = ['siswa', 'id_kelas']
+
+class MasterJurusanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MasterJurusan
+        fields = ['siswa', 'id_jurusan']
+
+class AbsensiSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Absensi
+        fields = ['checkin', 'checkout']
